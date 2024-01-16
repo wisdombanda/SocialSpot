e226bce031f5b2dbe9087749482e7faf1fe3dd05
Wisdom <coreeymichael@gmail.com>
2024-01-16 11:10:55 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'SocialSpot',
+  description: 'MAKE YOUR EVENTS,A SUCCESS BY, CONNECTING WITH,SOCIAL-SPOT!',
   icons: {
     icon: '/assets/images/logo.svg'
   }
--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image 
-            src="/assets/images/logo.svg" width={128} height={38}
-            alt="Evently logo" 
+            src="/assets/images/logo.png" width={188} height={98}
+            alt="SocialSpot logo" 
           />
         </Link>
 
